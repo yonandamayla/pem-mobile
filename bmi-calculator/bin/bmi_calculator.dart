3fc8d40330b7cae937adc58869b308f4f9cf9b5e
yonandamayla <yonandamayla555@gmail.com>
2025-09-18 15:39:42 +0700

    add: bmi-calculation

--- bmi-calculator/bin/bmi_calculator.dart
+++
@@ -1,5 +0,0 @@
-import 'package:bmi_calculator/bmi_calculator.dart' as bmi_calculator;
-
-void main(List<String> arguments) {
-  print('Hello world: ${bmi_calculator.calculate()}!');
-}
--- bmi-calculator/test/bmi_calculator_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bmi_calculator/bmi_calculator.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}

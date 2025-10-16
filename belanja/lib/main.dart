0aca97335250ba3af20552e0fa22a7f6bab397d6
yonandamayla <yonandamayla555@gmail.com>
2025-10-16 08:48:13 +0700

    Simplify main function by removing MyApp class and directly using MaterialApp

--- belanja/lib/main.dart
+++ belanja/lib/main.dart
@@ -3,25 +3,11 @@ import 'pages/home_page.dart';
 import 'pages/item_page.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shopping App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomePage(),
-        '/item': (context) => ItemPage(),
-      },
-    );
-  }
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => HomePage(),
+      '/item': (context) => ItemPage(),
+    },
+  ));
 }

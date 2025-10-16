bdaf1e25de725a2838aba8f4cd1a6652d09b42f2
yonandamayla <yonandamayla555@gmail.com>
2025-10-16 08:56:10 +0700

    Add item list initialization in HomePage class

--- belanja/lib/pages/home_page.dart
+++ belanja/lib/pages/home_page.dart
@@ -1,6 +1,12 @@
 ﻿import 'package:flutter/material.dart';
+import '../models/item.dart';
 
 class HomePage extends StatelessWidget {
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000)
+  ];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build

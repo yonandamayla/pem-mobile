ab01d197e0a5efdf51fc9be5684163143e95eddc
yonandamayla <yonandamayla555@gmail.com>
2025-10-16 09:40:30 +0700

    Refactor HomePage and ItemPage for code simplification and consistency

--- belanja/lib/pages/home_page.dart
+++ belanja/lib/pages/home_page.dart
@@ -2,17 +2,17 @@
 import '../models/item.dart';
 
 class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
   final List<Item> items = [
     Item(name: 'Sugar', price: 5000),
-    Item(name: 'Salt', price: 2000)
+    Item(name: 'Salt', price: 2000),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Shopping List'),
-      ),
+      appBar: AppBar(title: Text('Shopping List')),
       body: Container(
         margin: EdgeInsets.all(8),
         child: ListView.builder(
@@ -20,18 +20,16 @@ class HomePage extends StatelessWidget {
           itemCount: items.length,
           itemBuilder: (context, index) {
             final item = items[index];
-            return InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, '/item');
-              },
-              child: Card(
+            return Card(
+              child: InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, '/item');
+                },
                 child: Container(
                   margin: EdgeInsets.all(8),
                   child: Row(
                     children: [
-                      Expanded(
-                        child: Text(item.name),
-                      ),
+                      Expanded(child: Text(item.name)),
                       Expanded(
                         child: Text(
                           item.price.toString(),
--- belanja/lib/pages/item_page.dart
+++ belanja/lib/pages/item_page.dart
@@ -1,18 +1,13 @@
 ﻿import 'package:flutter/material.dart';
 
 class ItemPage extends StatelessWidget {
+  const ItemPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Item Detail'),
-      ),
-      body: Center(
-        child: Text(
-          'Detail Page',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+      appBar: AppBar(title: Text('Item Detail')),
+      body: Center(child: Text('Detail Page', style: TextStyle(fontSize: 24))),
     );
   }
 }

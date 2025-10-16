50386bb4f56ada22f3f4d32f2fae4a4cd03af4d0
yonandamayla <yonandamayla555@gmail.com>
2025-10-16 08:52:53 +0700

    Remove unused properties from Item class for simplification

--- belanja/lib/models/item.dart
+++ belanja/lib/models/item.dart
@@ -1,15 +1,6 @@
-class Item {
+﻿class Item {
   String name;
   int price;
-  String photo;
-  int stok;
-  double rating;
 
-  Item({
-    required this.name,
-    required this.price,
-    required this.photo,
-    required this.stok,
-    required this.rating,
-  });
+  Item({required this.name, required this.price});
 }
